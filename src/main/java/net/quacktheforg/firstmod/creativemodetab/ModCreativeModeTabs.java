56f549bb444d59475f8ad26431aab5aedc5b1efb
quack_theforg <teodorozafir@gmail.com>
2026-09-14 10:25:26 +1200

    added ruby tab

--- src/main/java/net/quacktheforg/firstmod/creativemodetab/ModCreativeModeTabs.java
+++ src/main/java/net/quacktheforg/firstmod/creativemodetab/ModCreativeModeTabs.java
@@ -6,22 +6,49 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.quacktheforg.firstmod.FirstMod;
 import net.quacktheforg.firstmod.item.ModItems;
 
-public class ModCreativeModeTabs {
-    public static final CreativeModeTab RUBY_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
-            Identifier.fromNamespaceAndPath(FirstMod.MOD_ID, "ruby_items"),
-            FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RUBY))
-                    .title(Component.translatable("creativemodetab.first-mod.ruby_items"))
-                    .displayItems((parameters, output) -> {
-                        output.accept(ModItems.RUBY);
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-                    }).build());
+public class ModCreativeModeTabs {
+
+    public static final CreativeModeTab RUBY_ITEM_TAB = registerTab(
+            "ruby_items",
+            () -> new ItemStack(ModItems.RUBY),
+            output -> output.accept(ModItems.RUBY)
+    );
+
+    private static CreativeModeTab registerTab(
+            String name,
+            Supplier<ItemStack> icon,
+            Consumer<CreativeModeTab.Output> items
+    ) {
+        ResourceKey<CreativeModeTab> key = ResourceKey.create(
+                BuiltInRegistries.CREATIVE_MODE_TAB.key(),
+                Identifier.fromNamespaceAndPath(FirstMod.MOD_ID, name)
+        );
+
+        CreativeModeTab tab = FabricCreativeModeTab.builder()
+                .icon(icon)
+                .title(Component.translatable(
+                        "creativemodetab." + FirstMod.MOD_ID + "." + name
+                ))
+                .displayItems((parameters, output) -> items.accept(output))
+                .build();
+
+        return Registry.register(
+                BuiltInRegistries.CREATIVE_MODE_TAB,
+                key,
+                tab
+        );
+    }
 
     public static void registerCreativeModeTabs() {
-        FirstMod.LOGGER.info("Registering Creative Mode Tabs for" + FirstMod.MOD_ID);
+        FirstMod.LOGGER.info("Registering Creative Mode Tabs for " + FirstMod.MOD_ID);
     }
 }
